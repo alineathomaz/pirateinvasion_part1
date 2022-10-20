1f972a9b724b6e370c54ed5a14ac885df5cc4bd2
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2022-10-20 13:23:12 -0300

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,22 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;    
+  }
+
+  //código de display() para exibir o corpo do canhão:
+  display(){
+    //código para criar o topo do canhão:
+
+
+
+
+
+    //código para criar a base do canhão:
+
+
+  }
+}
